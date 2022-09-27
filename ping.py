d3a1c177ed84502e3a75e2ec5a6a41c6d3bdca78
SullIsGaming <90643310+SullIsGaming@users.noreply.github.com>
2022-09-27 14:27:29 -0400

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify, request 
+from flask_httpauth import HTTPDigestAuth
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'message':'Page Not Here'}), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'message':'Something is Broke'}), 500
+
+@app.route('/ping', methods=['GET'])
+def index():
+    return jsonify({'message':'Running...'})
